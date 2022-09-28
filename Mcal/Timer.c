731fa6883aa7bc6f06df02a932d7152729bc14f7
ahmedhish <68624722+ahmedhish@users.noreply.github.com>
2022-09-29 00:42:00 +0200

    Added new driver for timers

--- Mcal/Timer.c
+++ Mcal/Timer.c
@@ -9,7 +9,7 @@ TIMER1_TAMR_R= 0x02;
 TIMER1_TAPR_R = 250-1; 
 TIMER1_TAILR_R = 64000-1 ; 
 TIMER1_ICR_R = 0x1;         
-TIMER1_IMR_R |=(1<<0); 
+TIMER1_IMR_R |=1; 
 TIMER1_CTL_R |= 0x01;        
 NVIC_EN0_R |= (1<<21); 
 }
