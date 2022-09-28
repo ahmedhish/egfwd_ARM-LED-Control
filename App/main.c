f05801fd288ab43c71bacc18932b02179e22eb21
ahmedhish <68624722+ahmedhish@users.noreply.github.com>
2022-09-29 00:40:18 +0200

    Update main.c

--- App/main.c
+++ App/main.c
@@ -1,15 +1,38 @@
 #include "..\Mcal\Inc\Dio.h"
-#include "..\Mcal\Inc\Timer.h"
-unsigned int T_ON=0;
-unsigned int T_OFF=0;
-unsigned int counter=0;
+#include "..\Mcal\Inc\GPT_interface.h"
+volatile unsigned int T_ON=0;
+volatile unsigned int T_OFF=0;
+volatile unsigned int counter=0;
+
+void LED_Blink(void)
+{
+	counter++;
+	//Ton=5 & Toff=3
+		#ifdef Testing
+		Dio_FlipChannel(F,pin1);
+		#else
+		if(counter <= T_ON)
+			Dio_WriteChannel(F,pin1,HIGH);
+		else
+			Dio_WriteChannel(F,pin1,LOW);
+		
+		
+		if(counter == (T_ON + T_OFF))
+			counter=0;
+		#endif
+}
+
 
 int main(void)
 {
 	Dio_Init(F,pin1,output);//red
 	Dio_Init(F,pin0,input);//sw1
 	Dio_Init(F,pin4,input);//sw2
-	Time1A_Delay();
+	GPT_Init();
+	GPT_EnableNotification(T1);
+	GPT_Notification_T1(LED_Blink);
+	GPT_StartTimer(T1,16000000);
+	
 	while(1)
 	{
 		if(~Dio_ReadChannel(F,pin0))
@@ -25,31 +48,7 @@ int main(void)
 				while(~Dio_ReadChannel(F,pin0));
 			}
 		}
-			
-		
 	}
 	
 	return 0;
-}
-
-void TIMER1A_Handler (void)
-{ 
-	counter++;
-	//Ton=5 & Toff=3
-	if(TIMER1_MIS_R & 0x1)
-	{
-		#ifdef Testing
-		Dio_FlipChannel(F,pin1);
-		#else
-		if(counter <= T_ON)
-			Dio_WriteChannel(F,pin1,HIGH);
-		else
-			Dio_WriteChannel(F,pin1,LOW);
-		
-		
-		if(counter == (T_ON + T_OFF))
-			counter=0;
-		#endif
-	}
-	TIMER1_ICR_R=1;
 }
